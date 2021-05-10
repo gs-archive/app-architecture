d5bc9ec6f51dbacd01b81be75a07b18f11b0e536
gülşah <33907502+gulsahsevinel@users.noreply.github.com>
2021-05-10 21:27:22 +0300

    dataBinding

--- app/src/main/java/com/gulsah/apparchitecture/MainActivity.kt
+++ app/src/main/java/com/gulsah/apparchitecture/MainActivity.kt
@@ -5,29 +5,17 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.gulsah.apparchitecture.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val viewModel : MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val counterText = findViewById<TextView>(R.id.counterText)
-        val addOneButton : Button = findViewById(R.id.addOneButton)
-        val addTenButton : Button = findViewById(R.id.addTenButton)
-
-
-        viewModel.counterString.observe(this){
-            //toString i kaldırdık
-            counterText.text = it
-        }
-
-        addOneButton.setOnClickListener {
-            viewModel.onAddOneButtonClick()
-        }
-        addTenButton.setOnClickListener {
-            viewModel.onAddTenButtonClick()
-        }
+        val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
+        // tüm liveData'ları otomatik olarak observe ediyor
+        binding.lifecycleOwner = this
     }
 }
